d952a76c39a8f9bbb5587b21a2bd2a0ffaaa3993
Xavier León <xleon.mail@gmail.com>
2014-10-09 15:59:29 +0200

    First commit

---
+++ FairIOController/src/FairIOController.java
@@ -0,0 +1,15 @@
+import org.apfloat.Apfloat;
+
+
+public class FairIOController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Apfloat f = new Apfloat(1345632530.0);
+		System.out.println(f);
+
+	}
+
+}
